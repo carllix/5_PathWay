6c7db03f6578fd9f0612313e76fd50f1ed8c45b1
Kurond <adityarahmadeni30@gmail.com>
2024-08-25 04:11:28 +0700

    style: set padding for card and text

--- src/app/(informasi)/lomba/page.tsx
+++ src/app/(informasi)/lomba/page.tsx
@@ -5,27 +5,29 @@ export default function Lomba() {
     <div className="p-5 max-w-5xl mx-auto">
 
       
-      {/* Search Bar */}
-      <div className="flex items-center max-w-xs border border-gray-300 rounded-lg overflow-hidden p-5">
-        {/* Search Field */}
-        <input
-          type="text"
-          placeholder="Search..."
-          className="flex-grow p-2 focus:outline-none bg-[#F0D78C]"
-        />
-
-        {/* Search Button */}
-        <button className="p-2 bg-[#F0D78C] text-white flex items-center justify-center">
-          <Image 
-            src="/InformationPage/search.svg" // Path to your SVG in the public directory
-            alt="Search"
-            width={20} // Adjust size as needed
-            height={20}
+      {/* Search Bar and filter container */}
+      <div className="flex items-center overflow-hidden p-5">
+        <div className="flex items-center max-w-xs border border-gray-300 rounded-lg overflow-hidden">  
+          {/* Search Field */}
+          <input
+            type="text"
+            placeholder="Search..."
+            className="flex-grow p-2 focus:outline-none bg-[#F0D78C]"
           />
-        </button>
+          {/* Search Button */}
+          <button className="p-2 bg-[#F0D78C] text-white flex items-center justify-center">
+            <Image 
+              src="/InformationPage/search.svg" // Path to your SVG in the public directory
+              alt="Search"
+              width={25} // Adjust size as needed
+              height={25}
+            />
+          </button>
+        </div>
       </div>
 
-      <h1>Berikut rekomendasi lomba untuk kamu</h1>
+      <h1 className='px-5'>Berikut rekomendasi lomba untuk kamu</h1>
+
       {/* Card Info Lomba */}
       <div className="grid grid-cols-3 gap-5 p-5">
         <div className="p-5 rounded-lg shadow-md" style={{ backgroundColor: "#CCE7F7" }}>
