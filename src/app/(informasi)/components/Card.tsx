ca354b806c65d54fe6fc03f4645c9aa735e503f9
Kurond <adityarahmadeni30@gmail.com>
2024-08-25 11:27:48 +0700

    feat: add card component

---
+++ src/app/(informasi)/components/Card.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export default function Card({
+    title,
+    description,
+}: {
+    title: string;
+    description: string;
+}) {
+    return (
+        <div className="flex flex-col rounded-lg shadow-md p-4 bg-[#CCE7F7]">
+            <Image 
+                src="/information-details.jpg" 
+                alt="poster" 
+                width={800} 
+                height={1034}
+                className="rounded-xl w-full h-80"
+            />
+            <p className="font-bold">{title}</p>
+            <p className="text-justify text-xs ">{description}</p>
+        </div>
+    );
+}
--- src/app/(informasi)/lomba/page.tsx
+++ src/app/(informasi)/lomba/page.tsx
@@ -7,6 +7,7 @@ import { IoMdSearch } from "react-icons/io";
 import InformasiNavigation from "../components/InformasiNavigation";
 import ModalLombaFilter from "../components/ModalLombaFilter";
 import { usePathname } from "next/navigation";
+import Card from "../components/Card";
 
 export default function Lomba() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -54,6 +55,23 @@ export default function Lomba() {
       </div>
 
       {/* List Lomba */}
+      <div className="px-4 mt6">
+        <p className="py-4">Berikut rekomendasi lomba untuk kamu</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2x1:grid-cols-6 gap-8">
+          <Card
+            title="Lomba 1"
+            description="Deskripsi lomba 1"
+          />
+          <Card
+            title="Lomba 1"
+            description="Deskripsi lomba 1"
+          />
+          <Card
+            title="Lomba 1"
+            description="Deskripsi lomba 1"
+          />
+        </div>
+      </div>
 
 
       {showModal && (
