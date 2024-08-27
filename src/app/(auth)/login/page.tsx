60960fe7c234f5b5daf3113817135dd093966fa1
carllix <carloangkisan21@gmail.com>
2024-08-27 11:56:17 +0700

    chore: auth folder structuring

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className="flex h-screen mx-auto justify-center items-center">
+      <div className="">
+        <p>Login</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <div className="flex h-screen mx-auto justify-center items-center">
+      <div className="">
+        <p>Signup</p>
+      </div>
+    </div>
+  );
+}
