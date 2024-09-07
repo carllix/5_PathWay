f68b48054645b6488b50c04ad1876bffd5c581e3
carllix <carloangkisan21@gmail.com>
2024-09-08 00:22:22 +0700

    style: change some style modal syarat

--- src/app/(informasi)/components/ModalSyarat.tsx
+++ src/app/(informasi)/components/ModalSyarat.tsx
@@ -15,12 +15,11 @@ export default function ModalSyarat({
     <div className="absolute top-24 left-1/2 -translate-x-1/2 w-4/5 max-w-sm text-[#F0D78C] bg-[#4F81C7] p-4 rounded-2xl text-sm">
       <p className="text-center font-bold text-xl">Syarat dan Ketentuan</p>
       <div className="mt-7 text-justify text-white">
-        {syarat && (
-          <p className="mb-3">
-            {syarat}
-          </p>
-        )}
-        <p className="text-start">For more information : <span className="underline underline-offset-4">{more_info}</span></p>
+        {syarat && <p className="mb-3">{syarat}</p>}
+        <p className="text-start">For more information :</p>
+        <p className="mt-1 underline underline-offset-4 break-all">
+          {more_info}
+        </p>
       </div>
       <div className="flex justify-center">
         <Button
